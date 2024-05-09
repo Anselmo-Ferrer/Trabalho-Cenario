a72fb293588ffdf541c3859f4404694259e28194
Anselmo Dev <Anselmodev@MacBook-Pro-de-Anselmo.local>
2024-05-09 17:17:06 -0300

    att

--- script.js
+++ script.js
@@ -3,25 +3,39 @@ const math = require('mathjs');
 // npm install mathjs
 
 
-let velocidade = 3; // velocidade encontrada entre a media das matriculas
+// velocidade encontrada entre as medias das matriculas = 3 m/s
+// função encontrada no geogebra = 0x^9 + 0x^8 - 0.0028x^7 + 0.1048x^6 - 2.4379x^5 + 36.016x^4 - 333.3024x^3 + 1816.4963x^2 - 5006.4931x + 4476.7823
 
-let funcao = '-0.0028x^7 + 0.1048x^6 - 2.4379x^5 + 36.016x^4 - 333.3024x^3 + 1816.4963x^2 - 5006.4931x + 4476.7823'; // Função encontrada no geogebra
-let x0 = 3;
+// obs: o numero de interações depende do palpite inicial
+// x0 = 1 -> 4 interações
+// x0 = 3 -> 11 interações
+// x0 = 5 -> 31 interações
+
+// função minimizada pelo javascript
+let funcao = '-0.0028x^7 + 0.1048x^6 - 2.4379x^5 + 36.016x^4 - 333.3024x^3 + 1816.4963x^2 - 5006.4931x + 4476.7823'; 
+let x0 = 3; // palpite inicial
 let tolerancia = 0.001; // tolerância de 1%
-let maxIteracoes = 1000; 
+let maxInteracoes = 1000; // maximo de interações 
 
 // Função para calcular o método de Newton-Raphson
-function newtonRaphson(funcao, velocidade, tolerancia, maxIteracoes, x0) {
-    let x = x0; // x0 = 3
+function newtonRaphson(funcao, tolerancia, maxInteracoes, x0) {
+    let x = x0; 
     let interacoes = 0;
     let diferenca = Infinity;
 
-    // loop para calcular o Newton-Raphson ate a diferença ser menor que 1%
-    while (interacoes < maxIteracoes) {
+    // Informacoes sobre a função no console
+    let derivada = math.derivative(funcao, 'x');
+    console.log(`Funcao: ${funcao}`)
+    console.log(`Derivada: ${derivada}`)
+    console.log(`x0 = ${x0}`)
+    console.log(`---------------------------------------------------`)
 
-        // transforma a função em uma expressão que o java script entenda
+    // loop para calcular o Newton-Raphson ate a diferença ser menor que 1% ou o número de interações ser 1000
+    while (interacoes < maxInteracoes) {
+
+        // transforma a função em uma expressão que o java script entenda atraves da blibioteca mathjs
         let fx = math.evaluate(funcao, { x });
-        // transforma a função em uma derivada e depois em um expressão que o javascript entenda
+        // transforma a função em uma derivada 
         let dfx = math.derivative(funcao, 'x').evaluate({ x });
         // calcula o newtonRaphson
         let deltaX =  x - (fx / dfx);
@@ -34,19 +48,23 @@ function newtonRaphson(funcao, velocidade, tolerancia, maxIteracoes, x0) {
 
         // x se torna o valor do resultado no ultimo newtonRaphson
         x = deltaX
+
         // incrementa mais 1 no numero de interações
         interacoes++;
 
         // se a diferenca for menor que o 1% loop para
         if (diferenca > -0.001 && diferenca < tolerancia) {
-            break
+            break;
         }
 
     }
 
+    console.log(`---------------------------------------------------`)
+
     return x;
 }
 
-const resultado = newtonRaphson(funcao, velocidade, tolerancia, maxIteracoes, x0)
+const resultado = newtonRaphson(funcao, tolerancia, maxInteracoes, x0)
 
+// imprime o instante aproximado
 console.log(`Instante aproximado em que o personagem atinge a velocidade de 3 m/s: ${resultado}`)
